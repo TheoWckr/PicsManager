195f7edfff5414ca62de51cbc7f7e5180d36cb81
pipchild <pipchild@free.fr>
2020-07-07 18:02:04 +0200

    delete photo

--- app/src/main/java/com/example/myapplication/EditPhotoActivity.kt
+++ app/src/main/java/com/example/myapplication/EditPhotoActivity.kt
@@ -1,13 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.myapplication.helpers.auth
+import com.example.myapplication.service.PhotoService
 import com.google.firebase.storage.StorageReference
 
 class EditPhotoActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
@@ -32,6 +32,15 @@ class EditPhotoActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
         // - replace the contents of the view with that element
         val btnPhotoId= findViewById<TextView>(R.id.namePhoto)
         btnPhotoId.text = gsReference.name.removeSuffix(".jpg")
+
+        val btnPhotoDelete= findViewById<Button>(R.id.delete_photo)
+        btnPhotoDelete.setOnClickListener {
+            gsReference.delete()
+            PhotoService.deletePhoto(photoId)
+            Toast.makeText(this, "Photo successfully deleted", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
