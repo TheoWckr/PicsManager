a5ee2c965027c64883dc3f48c833917e6ec07194
pipchild <pipchild@free.fr>
2020-07-07 10:43:39 +0200

    listview fix + click on list item to get to gallery

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.Button
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.myapplication.GalleryActivity
 import com.example.myapplication.R
 import com.example.myapplication.service.AlbumService
 import com.google.android.material.textfield.TextInputEditText
@@ -26,24 +28,7 @@ class DashboardFragment : Fragment() {
 
     ): View? {
         // use arrayadapter and define an array
-        val userAlbums = ArrayList<String>()
-        for (album in AlbumService.albumList) {
-            userAlbums.add(album.name )}
 
-        val arrayAdapter: ArrayAdapter<*>
-        val userAlbum = arrayOf(
-            userAlbums
-        )
-
-        var mListView = view?.findViewById<ListView>(R.id.list_album)
-        arrayAdapter = this.context?.let {
-            ArrayAdapter(
-                it,
-                android.R.layout.simple_list_item_1, userAlbum)
-        }!!
-        if (mListView != null) {
-            mListView.adapter = arrayAdapter
-        }
 
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
@@ -58,15 +43,10 @@ class DashboardFragment : Fragment() {
         // access the listView from xml file
 
         val button = root.findViewById<Button>(R.id.createAlbum)
-        button.setOnClickListener { createAlbum }
+        button.setOnClickListener { createAlbum() }
+
         return root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-
 
     fun createAlbum(){
         // get input text
@@ -78,4 +58,56 @@ class DashboardFragment : Fragment() {
         }
     }
 
-}}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userAlbums = ArrayList<String>()
+        for (album in AlbumService.albumList) {
+            userAlbums.add(album.name)
+        }
+
+        val arrayAdapter: ArrayAdapter<*>
+//        val userAlbum = arrayOf("caca", "pipi")
+//        val userAlbum = arrayOf(
+//            userAlbums
+//        )
+
+        val mListView = view.findViewById<ListView>(R.id.list_album)
+        arrayAdapter = this.context?.let {
+            ArrayAdapter(
+                it,
+                android.R.layout.simple_list_item_1, userAlbums
+            )
+        }!!
+        if (mListView != null) {
+            mListView.adapter = arrayAdapter
+        }
+
+
+        mListView.setOnItemClickListener { parent, view, position, id ->
+            val element =  parent.getItemAtPosition(position) as String // The item that was clicked
+            val intent = Intent(activity, GalleryActivity::class.java)
+            intent.putExtra("albumName", element)
+            startActivity(intent)
+        }
+
+//        fun createAlbum(){
+//            // get input text
+//            val albumName= activity?.findViewById<TextInputEditText>(R.id.albumNameInput)
+//            // save on server
+//            if (albumName != null) {
+//                AlbumService.albumCreate(albumName.text.toString())
+////            println("on est passé par là")
+//            }
+//        }
+    }
+
+
+
+    fun gallery(v : View){
+        val anotherIntent = Intent(activity, GalleryActivity::class.java)
+        anotherIntent.putExtra("albumName", "new album")
+        startActivity(anotherIntent)
+    }
+
+}
